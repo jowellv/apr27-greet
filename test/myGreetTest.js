cd2150a5b672c92838c98ccb03917aa68f8eb1af
Jowell Villasin <jowell.villasin@gmail.com>
2015-04-29 08:53:58 -0700

    add grunt cofig

--- test/myGreetTest.js
+++ test/myGreetTest.js
@@ -5,11 +5,11 @@ var greet = require('../myGreet');
 describe('greets people', function(){
   it('should greet people properly', function() {
     expect(greet('Naeun')).to.eql('hello Naeun');
-  })
+  });
   it('should read passed in command line args', function() {
     var args =  ["some name","ignored args","who am i?"];
 
 
     expect(greet(args)).to.eql('hello ' + args);
-  })
+  });
 });
